020dc0e894cf17a31e4acb8aa856cb441ee47837
Andrea F. Daniele <afdaniele@ttic.edu>
2020-11-18 16:10:22 -0600

    fixed tf_bridge

--- packages/multirobot/src/tf_bridge_node.py
+++ packages/multirobot/src/tf_bridge_node.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
+import threading
 
 import rospy
 
 from duckietown.dtros import DTROS, NodeType
 from dt_communication_utils import DTCommunicationGroup, ANYBODY_BUT_ME
-from dt_device_utils import get_device_tag_id
 
 from tf2_msgs.msg import TFMessage
 from geometry_msgs.msg import TransformStamped
-from tf2_ros import TransformBroadcaster
+from tf2_ros import TransformBroadcaster, StaticTransformBroadcaster
 
 
 class TFBridgeNode(DTROS):
+
+    PUBLISH_TF_STATIC_EVERY_SECS = 10
 
     def __init__(self):
         super(TFBridgeNode, self).__init__(
@@ -19,47 +21,82 @@ class TFBridgeNode(DTROS):
             node_type=NodeType.SWARM
         )
         # get static parameter - ~veh
-        self.robot_hostname = rospy.get_param('~veh', None)
-        if self.robot_hostname is None:
+        try:
+            self.robot_hostname = rospy.get_param('~veh')
+        except KeyError:
             self.logerr('The parameter ~veh was not set, the node will abort.')
             exit(1)
-        # get static parameter - ~veh
-        self.robot_type = rospy.get_param('~robot_type', None)
-        if self.robot_type is None:
+        # get static parameter - ~robot_type
+        try:
+            self.robot_type = rospy.get_param('~robot_type')
+        except KeyError:
             self.logerr('The parameter ~robot_type was not set, the node will abort.')
             exit(2)
         # create communication group
         self._group = DTCommunicationGroup("/tf", TransformStamped)
+        # create static tfs holder and access semaphore
+        self._static_tfs = {}
+        self._static_tfs_sem = threading.Semaphore(1)
         # create publishers
         self._tf_pub = self._group.Publisher()
+        self._tf_static_timer = rospy.Timer(
+            rospy.Duration(self.PUBLISH_TF_STATIC_EVERY_SECS),
+            self._publish_static_tfs
+        )
         # create local subscribers
         self._local_tf_sub = rospy.Subscriber(
-            '~tf',
+            '/tf',
             TFMessage,
             self._cb_local_tf,
-            queue_size=1
+            queue_size=20
+        )
+        self._local_tf_static_sub = rospy.Subscriber(
+            '/tf_static',
+            TFMessage,
+            self._cb_local_tf_static,
+            queue_size=20
         )
         # create global subscriber
         self._tf_broadcaster = None
+        self._tf_static_broadcaster = None
         if self.robot_type in ['duckietown']:
             self._global_tf_sub = self._group.Subscriber(self._cb_global_tf)
-            self._tf_broadcaster = TransformBroadcaster(queue_size=10)
+            self._tf_broadcaster = TransformBroadcaster()
+            self._tf_static_broadcaster = StaticTransformBroadcaster()
 
     def on_shutdown(self):
-        self._group.shutdown()
+        if self._group is not None:
+            self._group.shutdown()
 
     def _cb_local_tf(self, msg):
         for transform in msg.transforms:
-            publish = False
-            # robots can only publish what they have perceived themselves
-            if transform.header.frame_id.startswith(f'/{self.robot_hostname}'):
-                publish = True
-            # `duckietown` devices can ALSO publish TFs with /world as parent
-            if self.robot_type in ['duckietown'] and transform.header.frame_id == '/world':
-                publish = True
+            if not self._can_publish(transform):
+                continue
             # publish
-            if publish:
-                self._tf_pub.publish(transform, destination=ANYBODY_BUT_ME)
+            self._tf_pub.publish(transform, destination=ANYBODY_BUT_ME)
+
+    def _cb_local_tf_static(self, msg):
+        self._static_tfs_sem.acquire()
+        try:
+            for transform in msg.transforms:
+                if not self._can_publish(transform):
+                    continue
+                key = (transform.header.frame_id, transform.child_frame_id)
+                self._static_tfs[key] = transform
+        finally:
+            self._static_tfs_sem.release()
+
+    def _publish_static_tfs(self, *_):
+        tfs = []
+        self._static_tfs_sem.acquire()
+        try:
+            for transform in self._static_tfs.values():
+                tfs.append(transform)
+        finally:
+            self._static_tfs_sem.release()
+        # publish
+        for tf in tfs:
+            self._tf_pub.publish(tf, destination=ANYBODY_BUT_ME)
 
     def _cb_global_tf(self, msg, header):
         # make sure we are not getting boomerang messages (this should hot happen)
@@ -67,8 +104,21 @@ class TFBridgeNode(DTROS):
             self.logwarn('Received boomerang messages. This should not have happened.')
             return
         # forward TF locally
-        if self._tf_broadcaster:
-            self._tf_broadcaster.sendTransform(msg)
+        tf_bcaster = self._tf_broadcaster if msg.header.seq > 0 else self._tf_static_broadcaster
+        if tf_bcaster:
+            tf_bcaster.sendTransform(msg)
+
+    def _can_publish(self, transform):
+        if self.robot_type == 'duckietown':
+            # `duckietown` devices can ONLY publish TFs with /world as parent
+            if transform.header.frame_id.lstrip('/') != 'world':
+                return False
+        else:
+            # other robots can ONLY publish what they have perceived themselves
+            if not transform.header.frame_id.lstrip('/').startswith(f'{self.robot_hostname}/'):
+                return False
+        # ---
+        return True
 
 
 if __name__ == '__main__':
