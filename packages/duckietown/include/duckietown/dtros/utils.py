845d7e110afb5fffb942c9e5c53b8dd9b3853e5f
Andrea F. Daniele <afdaniele@ttic.edu>
2021-05-17 11:02:44 -0500

    minor

--- packages/duckietown/include/duckietown/dtros/utils.py
+++ packages/duckietown/include/duckietown/dtros/utils.py
@@ -10,10 +10,11 @@ def get_ros_handler(force=False):
     Note from Andrea D. to whoever sees this function.
 
     This is the story behind this function. ROS developers intentionally left no pointers for
-    the end-users to follow that point to some critical objects used by a ROS Node to talk to
-    the ROS Master. This was done so that the end-user does mess with the ROS environment.
+    the end-users to follow that would point to some critical objects used by a ROS Node to
+    interact with the ROS Master. This was done so that the end-user does not mess with the ROS
+    environment.
 
-    Though, if you look at the class `rospy.impl.masterslave.ROSHandler`, you will find that
+    If you look at the class `rospy.impl.masterslave.ROSHandler`, you will find that
     there are some functionalities that were never exposed to the end-user. Some of these are
     really useful, for example, did you know that there is a mechanism in place for subscribing
     to ROS params changes? People have built hacky ways to get around this (see
@@ -52,8 +53,9 @@ def get_ros_handler(force=False):
         __rh = rhs[0]
     if len(rhs) > 1:
         print('WARNING: Two or more ROS Handlers were found in memory. If you happen to see this, '
-              'please, post a message on (please, reopen the issue if closed): '
-              'https://github.com/duckietown/dt-ros-commons/issues/24.')
+              'please, post a message on: '
+              'https://github.com/duckietown/dt-ros-commons/issues/24.\n'
+              'Please, do reopen the issue if it is closed.')
     return __rh
 
 
