dcb901c89ee8cb371991d22a103d713f26d84bb5
Andrea F. Daniele <afdaniele@ttic.edu>
2021-12-20 16:33:32 -0600

    updated DuckiematrixLinkDescription message

--- packages/ros_http_api/include/dt_ros_api/actions/duckiematrix.py
+++ packages/ros_http_api/include/dt_ros_api/actions/duckiematrix.py
@@ -1,5 +1,5 @@
 import rospy
-from duckietown_msgs.msg import DuckiematrixConnectorsDescription
+from duckietown_msgs.msg import DuckiematrixLinkDescription
 from flask import Blueprint, request
 
 from dt_ros_api.utils import response_ok, response_error
@@ -9,7 +9,7 @@ duckiematrix = Blueprint('duckiematrix', __name__)
 
 _pub = rospy.Publisher(
     "~duckiematrix/connect",
-    DuckiematrixConnectorsDescription,
+    DuckiematrixLinkDescription,
     queue_size=10,
     latch=True
 )
@@ -23,25 +23,25 @@ def _duckiematrix_start():
     else:
         args = request.args
     # get name, and URIs
-    name = args.get("name", "local")
-    data_in = args.get("data_in", None)
-    data_out = args.get("data_out", None)
+    matrix = args.get("matrix", "local")
+    uri = args.get("uri", None)
+    entity = args.get("entity", None)
     # check parameters
-    if data_in is None:
-        return response_error("Parameter 'data_in' is required.")
-    if data_out is None:
-        return response_error("Parameter 'data_out' is required.")
+    if uri is None:
+        return response_error("Parameter 'uri' is required.")
+    if entity is None:
+        return response_error("Parameter 'entity' is required.")
     # make a ROS message with the info
-    msg = DuckiematrixConnectorsDescription(
-        name=name,
-        data_in_uri=data_in,
-        data_out_uri=data_out,
+    msg = DuckiematrixLinkDescription(
+        matrix=matrix,
+        uri=uri,
+        entity=entity,
     )
     # publish message
     _pub.publish(msg)
     # return current API duckiematrix
     return response_ok({
-        'name': name,
-        'data_in': data_in,
-        'data_out': data_out,
+        'matrix': matrix,
+        'uri': uri,
+        'entity': entity,
     })
